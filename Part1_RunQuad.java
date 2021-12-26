dbfc032851300e8e6ab9506ef078504e69f2141b
Joebanezair <49142653+joebanezair@users.noreply.github.com>
2021-12-26 09:43:02 +0800

    Update Part1_RunQuad.java

--- Part1_RunQuad.java
+++ Part1_RunQuad.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package TP;
 import java.util.*;
 public class RunQuad{
@@ -12,9 +8,25 @@ public class RunQuad{
           String input = sc.nextLine();
           if(input.equals("S")||input.equals("s")){
               Square s = new Square();
+              
+              String description = """
+                 A square : 
+                 - has 4 equal sides
+                 - has 4 right angles
+                 """;
+              System.out.print(description);
+              
               s.showDescription();
           }else if(input.equals("R")||input.equals("r")){
               Rectangle r = new Rectangle();
+              
+              String description = """
+                 A Rectangle : 
+                 - 2 opposite sides are equal.
+                 - has 4 right angles
+                 """;
+              System.out.print(description);
+              
               r.showDescription();
           }else if(input.equals("")||input.equals(null)){
             System.err.println("Please enter a value between R & S");
@@ -26,38 +38,23 @@ public class RunQuad{
 }
 
 class Quadrilateral{
-  public void showDescriptionR(){
-      String R = """
-                 A rectangle:
+  public void showDescription(){
+      String description = """
                  - has 4 sides
-                 - has 4 right angles
-                 - all angles 90 degrees 
-                 - two Opposite sides are equal
-                 - is Quadrilaterals
+                 - is quadrilaterall
                  """;
-      System.out.println(R);
-  }
-  public void showDescriptionS(){
-      String R = """
-                 A square : 
-                 - has 4 equal sides
-                 - has 4 right angles
-                 - all angles 90 degrees
-                 - all sides are equal
-                 -  is Quadrilateral
-                 """;
-      System.out.println(R);
+      System.out.print(description);
   }
 }
 class Rectangle extends Quadrilateral{
   public void showDescription(){
-     super.showDescriptionR();
+      super.showDescription();
   }
 }
 class Square extends Rectangle{
   @Override
   public void showDescription(){
-      super.showDescriptionS();
+      super.showDescription();
   }
 }
 //current goal x10
